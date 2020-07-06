4a49403d78cc5cf6ac3391fa9e6270b0fdcac490
djadji <sennegueye@gmail.com>
2020-07-06 00:13:34 +0000

    first commit

---
+++ src/app/cv-item/data/cv-items.data.ts
@@ -0,0 +1,9 @@
+import * as EN from './cv-items.data.en';
+import * as DE from './cv-items.data.de';
+import * as FR from './cv-items.data.fr';
+
+export const DATA = {
+  'FR': FR,
+  'EN': EN,
+  'DE': DE,
+};
